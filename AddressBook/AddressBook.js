be8ff59228d9c7eb5ca8313219d24e4ac315a2a2
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2022-01-02 14:09:58 +0530

    Validate The Contact Details That Going To Be Added

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -20,8 +20,41 @@ class Contact{
         this.phoneNumber = phoneNumber
     }
 }
-console.log("\nWelcome To The Address Book\n")
-console.log("Insert Contact Into Address Book")
-let newCotact = new Contact("Suhas","Bhor","Pune","Maharashtra","Pune",411057,"suhas@gmail.com",1234567890) 
-console.log(newCotact)
-console.log("Contact Added Successfully")
+
+//Initializing Function 
+function contactDetails(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
+    const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let firstName_Check = firstNamePattern.test(firstName);
+
+    const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let lastName_Check = lastNamePattern.test(lastName);
+
+    const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
+    let address_Check = addressPattern.test(address);
+
+    const statePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let state_Check = statePattern.test(state);
+
+    const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
+    let city_Check = cityPattern.test(city);
+
+    const zipCodePattern = /^[0-9]{3}[\s]{1}[0-9]{3}/;
+    let zipCode_Check = zipCodePattern.test(zipCode) ;
+
+    const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
+    let email_Check = emailPattern.test(email);
+
+    const phoneNumberPattern = /^[0-9]{2}[\s]{1}[0-9]{10}/;
+    let phoneNumber_Check = phoneNumberPattern.test(phoneNumber);
+
+        if(firstName_Check == true && lastName_Check == true && address_Check == true && state_Check == true && city_Check == true
+            && zipCode_Check == true && email_Check == true && phoneNumber_Check == true){
+            let newCotact = new Contact(firstName,lastName,address,state,city,zipCode,email,phoneNumber);
+            console.log("Contact Added Successfully");
+            console.log(newCotact);
+        }else{
+            throw 'Contact Details Are Invalid';
+        }
+}
+//Calling Fucntion
+contactDetails("Suhas","Bhor","Pune","Maharashtra","Pune","411 057","suhas@gmail.com","91 1234567891");
